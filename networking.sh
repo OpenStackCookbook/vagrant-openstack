a8620c2afcd3ae216b300ff7c901a30dea26334a
Kevin Jackson <uksysadmin@gmail.com>
2017-06-16 18:32:54 +0000

    Added config.yml to configure the deployment

--- networking.sh
+++ networking.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#sudo cp /vagrant/openstack.cfg /etc/network/interfaces.d/
 echo "source /etc/network/interfaces.d/*.cfg" >> /etc/network/interfaces
 
 IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}' | cut -d '.' -f4)
 
 sed -i "s/__IP__/${IP}/g" /etc/network/interfaces.d/openstack.cfg
-#/etc/init.d/networking restart
+
 ifdown eth1
 ifdown eth2
 ifdown eth3
+ifdown eth4
 ifup br-mgmt
 ifup br-vlan
 ifup br-vxlan
 ifup eth1
 ifup eth2
 ifup eth3
+ifup eth4
+ip addr flush dev eth1
+ip addr flush dev eth4
