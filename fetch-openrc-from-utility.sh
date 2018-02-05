e8e22f27cc92786e23866e5302cfc5a584436a34
Wojciech Sciesinski <wojciech@sciesinski.net>
2018-02-05 22:35:07 +0100

    Change logic of tooling scripts

--- fetch-openrc-from-utility.sh
+++ fetch-openrc-from-utility.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-if [[ $(hostname -s) == "controller-01" ]]
-then
+if [[ $(hostname -s) == "controller-01" ]]; then
+
 	# Gets openrc credentials from the utility container on controller-01
 	sudo ssh $(sudo lxc-ls -f | awk '/utility/ {print $1}') cat openrc > /vagrant/openrc
+
+elif [[ $(hostname -s)  == "openstack-client"  ]]; then
+
+	ssh controller-01 "sudo /vagrant/fetch-openrc-from-utility.sh"
+
+else
+
+	vagrant ssh controller-01 -c "sudo /vagrant/fetch-openrc-from-utility.sh"
+
 fi
