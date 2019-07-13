217bcadfc0427447ffa73090de487b10fa9c76d9
Kevin Jackson <uksysadmin@gmail.com>
2019-07-13 09:48:25 +0100

    Bumped up to OpenStack Rocky on #Ubuntu 18.04 Bionic

--- Vagrantfile
+++ Vagrantfile
@@ -30,7 +30,8 @@ Vagrant.configure("2") do |config|
   end
 
   # Defaults (VirtualBox)
-  config.vm.box = "velocity42/xenial64"
+  #config.vm.box = "velocity42/xenial64"
+  config.vm.box = "bento/ubuntu-18.04"
   config.vm.synced_folder ".", "/vagrant", type: "nfs"
 
   if config.vm.provider :vmware_workstation
--- networking.sh
+++ networking.sh
@@ -2,9 +2,11 @@
 
 echo "source /etc/network/interfaces.d/*.cfg" >> /etc/network/interfaces
 
-IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}' | cut -d '.' -f4)
+IP=$(ifconfig eth1 | awk '/inet\ / {print $2}' | cut -d '.' -f4)
 
 sed -i "s/__IP__/${IP}/g" /etc/network/interfaces.d/openstack.cfg
+
+#systemctl restart networking.service
 
 ifdown eth1
 ifdown eth2
